469a1521613c0292845553b7a763c78393666e28
alexi3rave <alexi3rave@gmail.com>
2023-09-29 01:08:01 +0300

    On the build.gradle encoding problem solved

---
+++ src/test/java/com/alexi3rave/ParametersTest.java
@@ -0,0 +1,17 @@
+package com.alexi3rave;
+
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParametersTest {
+
+    @DisplayName("Проверка города")
+    @ParameterizedTest(name = "{displayName} {0}")
+    @ValueSource (strings = {"Москва", "Таганрог", "Прочие города"})
+public void parametersTest(String city) {
+        Allure.parameter("Город", city);
+    }
+}
+
---
+++ src/test/java/com/alexi3rave/testAnnotatedWithLabel.java
@@ -0,0 +1,37 @@
+ package com.alexi3rave;
+
+ import com.alexi3rave.steps.WebSteps;
+ import io.qameta.allure.*;
+ import org.junit.jupiter.api.DisplayName;
+ import org.junit.jupiter.api.Test;
+
+
+ public class testAnnotatedWithLabel {
+
+
+     private static final String REPOSITORY = "alexi3rave/test";
+
+     private WebSteps steps = new WebSteps();
+
+     @Test
+     @DisplayName("Аннотационный тест с разметкой")
+     @Severity(SeverityLevel.BLOCKER)
+     @Owner("Alex Brave")
+     @Feature("Авторизация")
+     @Story("Авторизация через E-mail")
+
+     public void testGitHub() {
+         steps.OpenMainPage();
+         steps.takeSreenshot();
+         steps.FindRepository(REPOSITORY);
+         steps.takeSreenshot();
+         steps.GoToRepository(REPOSITORY);
+         steps.takeSreenshot();
+         steps.OpenGradleFolder();
+         steps.takeSreenshot();
+         steps.FindGradleFile();
+         steps.takeSreenshot();
+     }
+
+
+ }
