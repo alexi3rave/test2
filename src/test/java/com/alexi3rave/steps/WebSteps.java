14fb1e57fbee2fdfefe244d7a110263dfc1de784
alexi3rave <alexi3rave@gmail.com>
2023-09-28 19:01:32 +0300

    add screenshot to couple tests

--- src/test/java/com/alexi3rave/steps/WebSteps.java
+++ src/test/java/com/alexi3rave/steps/WebSteps.java
@@ -1,8 +1,13 @@
 package com.alexi3rave.steps;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -33,5 +38,9 @@ public class WebSteps {
     public void FindGradleFile() {
        $(By.linkText("build.gradle")).should(Condition.visible);
     }
-
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeSreenshot() {
+        final WebDriver driver = WebDriverRunner.getWebDriver();
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
 }
---
+++ src/test/java/com/alexi3rave/testAnnotated.java
@@ -0,0 +1,34 @@
+ package com.alexi3rave;
+
+ import com.alexi3rave.steps.WebSteps;
+ import com.codeborne.selenide.WebDriverRunner;
+ import com.codeborne.selenide.impl.Screenshot;
+ import io.qameta.allure.Attachment;
+ import org.junit.jupiter.api.Test;
+ import org.openqa.selenium.OutputType;
+ import org.openqa.selenium.TakesScreenshot;
+ import org.openqa.selenium.WebDriver;
+
+
+ public class testAnnotated {
+
+     private static final String REPOSITORY = "alexi3rave/test";
+
+     private WebSteps steps = new WebSteps();
+
+     @Test
+     public void testGitHub() {
+         steps.OpenMainPage();
+         steps.takeSreenshot();
+         steps.FindRepository(REPOSITORY);
+         steps.takeSreenshot();
+         steps.GoToRepository(REPOSITORY);
+         steps.takeSreenshot();
+         steps.OpenGradleFolder();
+         steps.takeSreenshot();
+         steps.FindGradleFile();
+         steps.takeSreenshot();
+     }
+
+
+ }
