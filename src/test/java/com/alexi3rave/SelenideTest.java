69e57001266baba336c057e78fe36a5f91b207dd
alexi3rave <alexi3rave@gmail.com>
2023-09-29 12:40:40 +0300

    add to SelenideTest 'SelenideLogger.addListener'

--- src/test/java/com/alexi3rave/SelenideTest.java
+++ src/test/java/com/alexi3rave/SelenideTest.java
@@ -1,15 +1,17 @@
 package com.alexi3rave;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideTest {
     @Test
     public void testGitHub() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 open ("https://github.com/");
 $(".search-input").click();
 $("#query-builder-test").setValue("alexi3rave/test");
