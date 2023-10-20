bca8867b41b7aed2eede87768f704406d154bf62
alexi3rave <144437542+alexi3rave@users.noreply.github.com>
2023-10-20 17:35:18 +0300

    added telegram notifications

--- src/test/java/com/alexi3rave/ParametersTest.java
+++ src/test/java/com/alexi3rave/ParametersTest.java
@@ -1,13 +1,14 @@
 package com.alexi3rave;
 
 import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @Tag("remote")
-
+@Disabled
 public class ParametersTest extends TestBase {
 
     @DisplayName("Проверка города")
